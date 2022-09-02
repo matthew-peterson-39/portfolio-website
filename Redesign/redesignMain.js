def58da5553e1a06caf64ac777c9c04572eacfa1
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-09-03 00:51:07 +0300

    Redesigned main page and bio.

--- Redesign/redesignMain.js
+++
@@ -1,6 +0,0 @@
-const nameText = document.querySelector('#name');
-
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    nameText.style.marginBottom = value * 2 + 'px';
-})
